2be725b967647d52e8926af604efce75cefdd7e0
bogdy9912 <bogdancons99@gmail.com>
2019-11-14 14:47:06 +0200

    no message

--- LabsHomeworks/Lab3/Lab3.ino
+++ LabsHomeworks/Lab3/Lab3.ino
@@ -1,20 +1,22 @@
 const int buzzerInPin = A0;
 const int ledPin = 13;
 const int buzzerOutPin = 10;
-
+const int buttonPin = 4;
 
 int buzzerValue = 0;
 int ledState = 0;
-
-
-const int threshold = 5;
+int buttonState = 1;
+const int threshold = 1;
+int currentTime = 0;
+int timer = 0;
+int ok=0;
 
 void setup() {
   
   pinMode(buzzerInPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   pinMode(buzzerOutPin, OUTPUT);
-  
   Serial.begin(9600);
 }
 
@@ -23,14 +25,24 @@ void loop() {
 
   if(buzzerValue!=0)
     Serial.println(buzzerValue);
-    
-  if (buzzerValue > threshold) {
+  
+  if (buzzerValue > threshold && ok == 0) 
+  {
+    ok=1;
     Serial.println("A batut ");  
-    ledState = !ledState;
-    tone(buzzerOutPin, buzzerValue);
+    ledState = 1;
+    timer=millis();
+  }
+    if ((millis()- timer) >= 5000 && ok == 1)
+      tone(buzzerOutPin, 600);
+      
+    buttonState = digitalRead(buttonPin);
+    
+    if (buttonState == LOW && ok == 1);
+      {
+        noTone(buzzerOutPin);  
+        ok=0;
+        ledState=0;
+      }
     digitalWrite(ledPin, ledState);
   }
-  delay(1000);
-  noTone(buzzerOutPin);
-  delay(10);
-}
